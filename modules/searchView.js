d63ebd8f47631fda5fe4e2972e8c6eac0083dc21
Mckenney <oluwarinolasam@gmail.com>
2020-07-17 19:50:54 +0100

    added no matching search functionality

--- modules/searchView.js
+++ modules/searchView.js
@@ -1,4 +1,8 @@
-import { setStyle, selectorAll, classAction } from './functionsUI.js';
+import {
+ setStyle, selectorAll, classAction, insertHtml, selector, setProp,
+} from './functionsUI.js';
+import DOMStrings from './DOMStrings.js';
+const { contactsDiv } = DOMStrings;
 
 const filterUIContacts = (filteredContacts) => {
     if (!filteredContacts) {
@@ -9,6 +13,9 @@ const filterUIContacts = (filteredContacts) => {
             }, 0);
         }
         [...selectorAll('.contact_class')].map((el) => setStyle(el, 'display', 'flex'));
+        if (selector('.no-matching-contacts')) {
+            setProp(selector('.no-matching-contacts'), 'outerHTML', '');
+        }
         return;
     }
     for (const el of [...selectorAll('.contact_div')]) {
@@ -18,12 +25,18 @@ const filterUIContacts = (filteredContacts) => {
         }, 0);
     }
     [...selectorAll('.contact_class')].map((el) => setStyle(el, 'display', 'flex'));
+    if (selector('.no-matching-contacts')) {
+        setProp(selector('.no-matching-contacts'), 'outerHTML', '');
+    }
 
     for (const el of filteredContacts) {
         classAction(el, 'remove', 'appear');
         const howManyRemains = [...el.parentNode.children].filter((ch) => ch.style.display !== 'none').length;
         if (howManyRemains < 2) {
             setStyle(el.parentNode.parentNode, 'display', 'none');
+            if ([...selectorAll('.contact_class')].every((cc) => cc.style.display === 'none')) {
+                insertHtml(contactsDiv, 'afterbegin', '<div class="no-matching-contacts">No matching Contacts</div>');
+            }
         } else {
             setStyle(el, 'display', 'none');
         }
